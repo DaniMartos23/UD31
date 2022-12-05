ef90c6e9567fcb920e1c8cb9034055e976909363
Dani Martos <dani.martos69@gmail.com>
2022-12-05 17:26:07 +0100

    ejercicio4 a medias

---
+++ Ejercicio4/js/codigo.js
@@ -0,0 +1,20 @@
+
+function clicparrafo1() {
+    alert('se presionó el párrafo fuera de las tablas');
+}
+
+function clicparrafotabla1_2() {
+    alert('se presionó el párrafo 2 contenido en la primera tabla');
+}
+
+function clicparrafotabla1() {
+    alert('se presionó el párrafo 1 contenido en la primera tabla');
+}
+
+function clicparrafotabla2_2() {
+    alert('se presionó el párrafo 2 contenido en la segunda tabla');
+}
+
+function clicparrafotabla2() {
+    alert('se presionó el párrafo 1 contenido en la segunda tabla');
+}
